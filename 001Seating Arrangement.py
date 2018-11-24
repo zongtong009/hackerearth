410a66ea99d69cbffd93e689177a2f4865a3dcda
zongtong009 <zongtongguilai1@gmail.com>
2018-11-25 03:47:04 +0800

    normal update

--- 001Seating Arrangement.py
+++ 001Seating Arrangement.py
@@ -1,21 +1,21 @@
-# Akash and Vishal are quite fond of travelling. They mostly travel by railways. 
-# They were travelling in a train one day and they got interested in the seating 
-# arrangement of their compartment. The compartment looked something like 
-# 1	2	3	||	||	4	5	6
-			# ||	||
-# 12	11	10	||	||	9	8	7
-# WS 	MS	AS	 过道	AS 	MS	WS
-# So they got interested to know the seat number facing them and the seat type 
-# facing them. The seats are denoted as follows : 
+# Akash and Vishal are quite fond of travelling. They mostly travel by railways.
+# They were travelling in a train one day and they got interested in the seating
+# arrangement of their compartment. The compartment looked something like
+# 1 2   3   ||  ||  4   5   6
+# ||    ||
+# 12    11  10  ||  ||  9   8   7
+# WS    MS  AS   过道 AS  MS  WS
+# So they got interested to know the seat number facing them and the seat type
+# facing them. The seats are denoted as follows :
 
 # Window Seat : WS
 # Middle Seat : MS
-# Aisle Seat : AS	
+# Aisle Seat : AS
 
-# INPUT:						OUPUT:
-	# 2						19 WS
-	# 18						45 AS
-	# 40
+# INPUT:                        OUPUT:
+# 2                     19 WS
+# 18                        45 AS
+# 40
 a = eval(input())
 test_dict = {
     1: 'WS', 6: 'WS', 7: 'WS', 12: 'WS',
@@ -23,12 +23,14 @@ test_dict = {
     3: 'AS', 4: 'AS', 9: 'AS', 10: 'AS',
 }
 for i in range(a):
-    m = eval(input())  
+    m = eval(input())
     quotient = m//12  # 商
     remainder = m % 12  # 余数
     if remainder==0:
-	    remainder=12
-	    quotient-=1
+      remainder=12
+      quotient-=1
     m_value=test_dict[remainder]
     m_correspond = quotient*12+13-remainder
     print(m_correspond, m_value)
+
+print(1)
---
+++ sublime_test.py
@@ -0,0 +1,7 @@
+n = 0
+print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+while True:
+    n += 1
+    print('sdfsg')
+    if n>100000:
+    	break
